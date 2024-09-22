1e9efa8445fcb9caa9edab1cf36bc88c3a5948c6
Rafael Rodrigues <130710250+Slyua7@users.noreply.github.com>
2024-09-22 15:08:21 -0300

    predios por departamento

--- backend/predios/views.py
+++ backend/predios/views.py
@@ -6,6 +6,10 @@ class PredioCreateListView(generics.ListCreateAPIView):
     queryset = Predio.objects.all()
     serializer_class = PredioSerializer
 
+    def get_queryset(self):
+        departamento_id = self.kwargs['departamento_id']
+        return Predio.objects.filter(departamentos__id=departamento_id)
+
 class PredioRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Predio.objects.all()
     serializer_class = PredioSerializer
